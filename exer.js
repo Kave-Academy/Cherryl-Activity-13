593dd8fae3e88f60ad7439d09ff58c4082c01e9e
Cherryl - Kave Blocks <cherryl@kaveblocks.com>
2022-02-25 17:07:40 +0800

    I created another variables to hold the inputedNumber then used the split() to split the string into an array and call the array per index

--- exer.js
+++ exer.js
@@ -32,6 +32,7 @@
 function run(){
     let num, sum, total = 0;
     let inputNumber = document.getElementById("number").value;
+    let splitNumber = inputNumber.split("");
     num = inputNumber;
 
     while(num>0){
@@ -43,11 +44,11 @@ function run(){
     if (inputNumber == total){
         document.getElementById("result").innerHTML = "  " 
         + inputNumber + " is an Armstrong Number since " 
-        + sum + "**3 + " + sum + "**3 + " + sum + "**3 = " + total + "!";
+        + splitNumber[0] + "**3 + " + splitNumber[1] + "**3 + " + splitNumber[2] + "**3 = " + total + "!";
     }else {
-        document.getElementById("result").innerHTML = "The number: " 
+        document.getElementById("result").innerHTML = " " 
         + inputNumber + " is NOT Armstrong Number since " 
-        + sum + "**3 + " + sum + "**3 + " + sum + "**3 = " + total + "!";
+        + splitNumber[0] + "**3 + " + splitNumber[1] + "**3 + " + splitNumber[2] + "**3 = " + total + "!";
     }
      
 }
