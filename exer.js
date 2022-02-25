84b93caae76d308d58ca6df4f398f684dfabe618
Cherryl - Kave Blocks <cherryl@kaveblocks.com>
2022-02-25 17:02:03 +0800

    git the solution and display the inputed number in the user interface. I just displayed the first[0] array.

--- exer.js
+++ exer.js
@@ -1,8 +1,53 @@
-function run() {
+// function run(){
+//     num = document.getElementById("number").value;
+//     var Number = num;
+//     var digits=0;
 
-    let input = document.querySelector("input").value;
+//     // Finding the number of digits
+//     while(num > 0){
+//     digits = digits + 1;
+//     num = parseInt(num/10);
+//     }
+//     num = Number;
+//     var sum = 0;
+
+//     // calculating sum
+//     while(num > 0) {
+//     var digit = num%10;
+//     sum = sum + Math.pow(digit, digits);
+//     num = parseInt(num/10);
+//     }
+
+//     // checking sum with original number
+//     if(sum == Number){
+//     document.getElementById("result").style.color = "blue";
+//     document.getElementById("result").innerHTML = "The number: " + Number + " is Armstrong Number";
+//     }else{
+//     document.getElementById("result").style.color = "red";
+//     document.getElementById("result").innerHTML = "The number: " + Number + " is NOT Armstrong Number";
+//     }
+//     }
 
 
-    document.getElementById("output").innerHTML = input;
+function run(){
+    let num, sum, total = 0;
+    let inputNumber = document.getElementById("number").value;
+    num = inputNumber;
 
+    while(num>0){
+        sum = num % 10;
+        num = parseInt(num/10);
+        total =  total + (sum*sum*sum);
+    }
+
+    if (inputNumber == total){
+        document.getElementById("result").innerHTML = "  " 
+        + inputNumber + " is an Armstrong Number since " 
+        + sum + "**3 + " + sum + "**3 + " + sum + "**3 = " + total + "!";
+    }else {
+        document.getElementById("result").innerHTML = "The number: " 
+        + inputNumber + " is NOT Armstrong Number since " 
+        + sum + "**3 + " + sum + "**3 + " + sum + "**3 = " + total + "!";
+    }
+     
 }
