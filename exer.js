d656f3233e15fb8cd1b592229454a7e6e3a2a72d
Cherryl - Kave Blocks <cherryl@kaveblocks.com>
2022-03-04 09:59:37 +0800

    replaced the button text into Read More and erase the id since they have the same IDName on the lower button

--- exer.js
+++ exer.js
@@ -5,20 +5,43 @@ function run(){
     let splitNumber = inputNumber.split("");
     num = inputNumber;
 
-    while(num>0){
+    while(num > 0){
         sum = num % 10;
         num = parseInt(num/10);
         total =  total + (sum*sum*sum);
     }
 
     if (inputNumber == total){
-        document.getElementById("result").innerHTML = "  " 
-        + inputNumber + " is an Armstrong Number since " 
-        + splitNumber[0] + "**3 + " + splitNumber[1] + "**3 + " + splitNumber[2] + "**3 = " + total + "!";
+        document.getElementById("result").innerHTML = "  " + inputNumber + " is an Armstrong Number since " + splitNumber[0] + "**3 + " + splitNumber[1] + "**3 + " + splitNumber[2] + "**3 = " + total + "!";
     }else {
-        document.getElementById("result").innerHTML = " " 
-        + inputNumber + " is NOT Armstrong Number since " 
-        + splitNumber[0] + "**3 + " + splitNumber[1] + "**3 + " + splitNumber[2] + "**3 = " + total + "!";
+        document.getElementById("result").innerHTML = " " + inputNumber + " is NOT Armstrong Number since " + splitNumber[0] + "**3 + " + splitNumber[1] + "**3 + " + splitNumber[2] + "**3 = " + total + "!";
     }
      
 }
+
+
+
+// function run(){
+//     let num, sum, total = 0;
+//     let inputNumber = document.getElementById("number").value;
+//     let splitNumber = inputNumber.split("");
+//     num = inputNumber;
+
+//     while(num>0){
+//         sum = num % 10;
+//         num = parseInt(num/10);
+//         total =  total + (sum*sum*sum);
+//     }
+
+//     if (inputNumber == total){
+//         document.getElementById("result").innerHTML = "  " 
+//         + inputNumber + " is an Armstrong Number since " 
+//         + splitNumber[0] + "**3 + " + splitNumber[1] + "**3 + " + splitNumber[2] + "**3 = " + total + "!";
+//     }else {
+//         document.getElementById("result").innerHTML = " " 
+//         + inputNumber + " is NOT Armstrong Number since " 
+//         + splitNumber[0] + "**3 + " + splitNumber[1] + "**3 + " + splitNumber[2] + "**3 = " + total + "!";
+//     }
+//     console.log(inputNumber)
+     
+// }
